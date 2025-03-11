77cb6e38b40c6ea8ffc5e7db4e49d29423b07c47
Thorsten Klöhn <thorstenkloehn@gmail.com>
2025-03-11 22:08:31 +0100

    Aktualisiere .gitignore und entferne die Datei WebsiteGolang; füge Kommentare zur Funktion routereinrichtung hinzu

--- routereinrichtung.go
+++ routereinrichtung.go
@@ -1,7 +1,8 @@
 package main
 
 import "net/http"
-
+// routereinrichtung ist eine Funktion, die einen http.ServeMux zurückgibt, der die Routen für die Anwendung einrichtet.
+// Die Funktion gibt einen http.ServeMux zurück, der die Routen für die Anwendung einrichtet.
 func routereinrichtung() * http.ServeMux {
 		server := http.NewServeMux()
 		fs := http.FileServer(http.Dir("static"))
